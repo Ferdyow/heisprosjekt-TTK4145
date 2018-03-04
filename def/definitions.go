c338a00eb914adbb4a734cea784a7da78295fd61
sanntidsal <sanntidsal@ntnu.no>
2018-03-04 18:14:59 +0100

    atrocities have happened

--- def/definitions.go
+++ def/definitions.go
@@ -5,15 +5,23 @@ const (
 	NUMB_HALL_BUTTONS = 2
 	NUMB_BUTTONS = 3
 
+	//Behaviour
 	IDLE      = 0
 	MOVING    = 1
 	DOOR_OPEN = 2
 
+	//Direction
 	DOWN = -1
 	STOP = 0
 	UP   = 1
 
+	//ButtonType
 	BUTTON_DOWN = 0
 	BUTTON_UP = 1
 	BUTTON_CAB = 2
 )
+
+type  Button struct {
+	Floor int;
+	Buttontype int;
+}
--- localFSM/localFSM.go
+++ localFSM/localFSM.go
@@ -5,7 +5,24 @@ import (
 	//"fmt"
 	//"time"
 	//"../network/peers"
-	//"../states"
-	//"../def"
+	"../states"
+	"../def"
 )
 
+
+
+func handleOrderAssigned(buttonCh <-chan def.Button, state states.States){
+	for{ //FOREVER
+		button := <-buttonCh;
+		switch state.Behaviour{
+		case def.DOOR_OPEN:
+			if state.Floor == button.Floor{
+				//Restart the timer
+			} else {
+				//set the fucking order
+			}
+			case 
+
+		}
+	}
+}
